78288a6d0ed1df2e7b8101f50e37ada050769e12
Tusman4ik <tusman4ik@gmail.com>
2023-11-26 14:44:49 +0300

    feach: create base structure

---
+++ src/main/java/com/skillforge/SkillforgeApplication.java
@@ -0,0 +1,14 @@
+package com.skillforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillforgeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkillforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillforge/SkillforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillforgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
